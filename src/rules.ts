9adcda947bdad59a069ad635fb39b8c57a580b63
Yuki Tanaka <uki00a@gmail.com>
2025-11-01 15:37:55 +0900

    feat: add `ban-allow-all` rule (#8)

--- src/rules.ts
+++ src/rules.ts
@@ -1,31 +1,90 @@
+import { parseArgsStringToArgv } from "string-argv";
 import type { JSONPath, Node } from "jsonc-parser";
-import { getNodeValue } from "jsonc-parser";
+import { getNodePath, getNodeValue } from "jsonc-parser";
 import type { DenoConfigurationFileSchema } from "../generated/config-file.v1.ts";
 
-type LintRuleTag = "recommended" | "security";
+type LintRuleTag =
+  | "recommended"
+  | "security"
+  | "permissions";
 
-interface LintProblem {
+interface LintReport {
   message: string;
+  node?: Node;
+}
+
+export interface LintReporter {
+  report(data: LintReport): void;
 }
 
 export interface LintRule {
   id: string;
   tags: Array<LintRuleTag>;
-  lint(node: Node | undefined): LintProblem | null;
+  lint(reporter: LintReporter, node: Node | undefined): void;
   paths(): Array<JSONPath>;
 }
 
+const kTasks = "tasks" satisfies keyof DenoConfigurationFileSchema;
+type TaskDefinition = Exclude<
+  NonNullable<DenoConfigurationFileSchema["tasks"]>[string],
+  string
+>;
+
+export const banAllowAll: LintRule = {
+  id: "ban-allow-all",
+  tags: ["recommended", "security", "permissions"],
+  paths: () => [
+    // TODO: Support `permissions`
+    [kTasks],
+  ],
+  lint(reporter, node) {
+    if (node == null) {
+      return null;
+    }
+    const flagsToBan = ["--allow-all", "-A"];
+    const path = getNodePath(node);
+    if (path[0] === kTasks && node.type === "object") {
+      for (
+        let i = 0, length = node.children?.length ?? 0;
+        i < length;
+        i++
+      ) {
+        const propertyNode = node.children?.[i];
+        if (propertyNode == null) continue;
+        if (propertyNode.type !== "property") continue;
+
+        const [, taskNode] = propertyNode.children ?? [];
+        if (taskNode == null) continue;
+        if (taskNode.type !== "string" && taskNode.type !== "object") continue;
+        const taskNodeValue = getNodeValue(taskNode);
+        const task = typeof taskNodeValue === "object"
+          ? (taskNodeValue as TaskDefinition).command
+          : taskNodeValue as string;
+        if (task == null) continue;
+        const args = parseArgsStringToArgv(task);
+        if (args.some((x) => flagsToBan.includes(x))) {
+          reporter.report({
+            node: taskNode,
+            message: `${flagsToBan.join("/")} should not be used`,
+          });
+        }
+      }
+    }
+    return null;
+  },
+};
 export const requireLockfile: LintRule = {
   id: "require-lockfile",
   tags: ["recommended", "security"],
   paths: () => [
     ["lock" satisfies keyof DenoConfigurationFileSchema],
   ],
-  lint(node) {
+  lint(reporter, node) {
     if (node != null && getNodeValue(node) === false) {
-      return { message: "A lockfile should be enabled" };
+      reporter.report({
+        message: "A lockfile should be enabled",
+      });
     }
-    return null;
   },
 };
 export const requireMinimumDependencyAge: LintRule = {
@@ -34,9 +93,11 @@ export const requireMinimumDependencyAge: LintRule = {
   paths: () => [
     ["minimumDependencyAge" satisfies keyof DenoConfigurationFileSchema],
   ],
-  lint(node) {
-    return node == null
-      ? { message: "`minimumDependencyAge` should be configured" }
-      : null;
+  lint(reporter, node) {
+    if (node == null) {
+      reporter.report({
+        message: "`minimumDependencyAge` should be configured",
+      });
+    }
   },
 };
