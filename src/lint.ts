23fd40cf6e9995e65688f31cf153f06f586dccf6
uki00a <uki00a@gmail.com>
2025-11-01 15:28:10 +0900

    feat: add `ban-allow-all` rule

--- src/lint.ts
+++ src/lint.ts
@@ -1,27 +1,36 @@
 import type { JSONPath } from "jsonc-parser";
 import { findNodeAtLocation, parseTree } from "jsonc-parser";
 import { LinesAndColumns } from "lines-and-columns";
-import type { LintRule } from "./rules.ts";
-import { requireLockfile, requireMinimumDependencyAge } from "./rules.ts";
+import type { LintReporter, LintRule } from "./rules.ts";
+import {
+  banAllowAll,
+  requireLockfile,
+  requireMinimumDependencyAge,
+} from "./rules.ts";
 
-interface Diagnostic {
+export interface Diagnostic {
   id: string;
   message: string;
   line?: number;
   column?: number;
 }
 
+export interface LintOptions {
+  include?: Array<string>;
+}
+
 export function lintText(
   configAsText: string,
+  options?: LintOptions,
 ): Array<Diagnostic> {
-  const diagnostics: Array<Diagnostic> = [];
   const tree = parseTree(configAsText);
   if (tree == null) return [];
   const lines = new LinesAndColumns(configAsText);
-  const rules = [
+  const rules = determineRules([
+    banAllowAll,
     requireLockfile,
     requireMinimumDependencyAge,
-  ];
+  ], options);
   const rulesGroupedByPath: Record<string, {
     rules: Array<LintRule>;
     path: JSONPath;
@@ -33,24 +42,42 @@ export function lintText(
       rulesGroupedByPath[key].rules.push(rule);
     }
   }
+  const diagnostics: Array<Diagnostic> = [];
   for (const { rules, path } of Object.values(rulesGroupedByPath)) {
     const node = findNodeAtLocation(tree, path);
     for (const rule of rules) {
-      const maybeProblem = rule.lint(node);
-      if (maybeProblem) {
-        const maybeLocation = node && lines.locationForIndex(node.offset);
-        const line = maybeLocation?.line ? maybeLocation.line + 1 : undefined;
-        const column = maybeLocation?.column
-          ? maybeLocation.column + 1
-          : undefined;
-        diagnostics.push({
-          ...maybeProblem,
-          id: rule.id,
-          line,
-          column,
-        });
-      }
+      const reporter: LintReporter = {
+        report(data) {
+          const { node, ...problem } = data;
+          const maybeLocation = node && lines.locationForIndex(node.offset);
+          const line = maybeLocation?.line ? maybeLocation.line + 1 : undefined;
+          const column = maybeLocation?.column
+            ? maybeLocation.column + 1
+            : undefined;
+          diagnostics.push({
+            ...problem,
+            id: rule.id,
+            line,
+            column,
+          });
+        },
+      };
+      rule.lint(reporter, node);
     }
   }
   return diagnostics;
+}
+
+function determineRules(
+  rules: Array<LintRule>,
+  options?: LintOptions,
+): Array<LintRule> {
+  if (options == null) {
+    return rules;
+  }
+  if (options.include == null || options.include.length === 0) {
+    return rules;
+  }
+  const { include: ruleIds } = options;
+  return rules.filter((x) => ruleIds.includes(x.id));
 }
---
+++ tests/lint.test.ts
@@ -0,0 +1,42 @@
+import assert from "node:assert/strict";
+import { lintText } from "../src/lint.ts";
+
+Deno.test({
+  name: "lintText()",
+  permissions: "none",
+  fn: async (t) => {
+    await t.step("ban-allow-all", () => {
+      const actual = lintText(
+        `{
+  "tasks": {
+    "simple:ok": "deno run --allow-read=testdata src/cli.ts",
+    "simple:ng": "deno run --allow-all src/cli.ts",
+    "complex:ok": {
+      "command": "deno run --allow-net=localhost src/cli.ts"
+    },
+    "complex:ng": {
+      "description": "This includes -A",
+      "command": "deno-run -A src/cli.ts"
+    }
+  }
+}`,
+        { include: ["ban-allow-all"] },
+      );
+      const expected = [
+        {
+          id: "ban-allow-all",
+          message: "--allow-all/-A should not be used",
+          line: 4,
+          column: 18,
+        },
+        {
+          id: "ban-allow-all",
+          message: "--allow-all/-A should not be used",
+          line: 8,
+          column: 19,
+        },
+      ];
+      assert.deepEqual(actual, expected);
+    });
+  },
+});
