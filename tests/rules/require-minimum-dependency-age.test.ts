9ea951eba16dcdd7c5c41c27cf49bcae3f668f29
uki00a <uki00a@gmail.com>
2026-06-27 15:06:20 +0900

    feat(require-minimum-dependency-age): skip linting on Deno >= 2.9.0
    NOTE: `minimumDependencyAge` is enabled by default starting in Deno 2.9.0.

--- tests/rules/require-minimum-dependency-age.test.ts
+++ tests/rules/require-minimum-dependency-age.test.ts
@@ -1,10 +1,41 @@
 import assert from "node:assert/strict";
-import { applyFixes, lintAndFixText } from "../../src/lint.ts";
+import { applyFixes, lintAndFixText, lintText } from "../../src/lint.ts";
+import type { Diagnostic } from "../../src/lint.ts";
 
 Deno.test({
   name: "require-minimum-dependency-age",
   permissions: "none",
   fn: async (t) => {
+    await t.step(
+      "allows `minimumDependencyAge` to be omitted if the Deno version is greater than or equal to 2.9.0",
+      () => {
+        const given = "{}";
+        const actual = lintText(given, {
+          include: ["require-minimum-dependency-age"],
+          denoVersion: "2.9.0",
+        });
+        const expected: Array<Diagnostic> = [];
+        assert.deepEqual(actual, expected);
+      },
+    );
+
+    await t.step("encourages defining `minimumDependencyAge`", () => {
+      const given = "{}";
+      const actual = lintText(given, {
+        include: ["require-minimum-dependency-age"],
+        denoVersion: "2.8.3",
+      });
+      const expected: Array<Diagnostic> = [
+        {
+          id: "require-minimum-dependency-age",
+          message: "`minimumDependencyAge` should be configured",
+          line: undefined,
+          column: undefined,
+        },
+      ];
+      assert.deepEqual(actual, expected);
+    });
+
     await t.step("supports `--fix`", () => {
       const given = `{}`;
       const { fixes, unfixableDiagnostics } = lintAndFixText(given, {
